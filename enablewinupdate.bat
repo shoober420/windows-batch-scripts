3cca91104b6879f92e45ee1b4c5c0e092adcb4cc
Vincent Grande <shoober420@gmail.com>
2023-06-03 11:01:22 -0700

    Create enablewinupdate.bat

---
+++ enablewinupdate.bat
@@ -0,0 +1,15 @@
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wisvc" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "2" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "2" /f
+
+net start UsoSvc
+net start wisvc
+net start wuauserv
+net start BITS
+net start TrustedInstaller
+net start WaaSMedicSvc
+
+PAUSE
