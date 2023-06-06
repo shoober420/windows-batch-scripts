bef4882f8733245ae2cb44ae50ddcbdad0df2853
Vincent Grande <shoober420@gmail.com>
2023-06-06 14:47:47 -0700

    Update and rename disableappx.bat to disableui.bat

---
+++ disableui.bat
@@ -0,0 +1,17 @@
+rem Ultra Minimal disable UI
+net stop AppXSvc
+net stop camsvc
+net stop UdkUserSvc
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\AppXSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\camsvc" /v "Start" /t REG_DWORD /d "4" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\UdkUserSvc" /v "Start" /t REG_DWORD /d "4" /f
+
+rem Application Frame Host (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "%WinDir%\System32\ApplicationFrameHost.exe"
+icacls "%WinDir%\System32\ApplicationFrameHost.exe" /grant:r %username%:F
+taskkill /im ApplicationFrameHost.exe /f
+rem del "%WinDir%\System32\ApplicationFrameHost.exe" /s /f /q
+ren "%WinDir%\System32\ApplicationFrameHost.exe" "ApplicationFrameHost.exe.bak"
+
+PAUSE
