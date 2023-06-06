70d4019281f29bc1bfd9172d846454c2a5778d83
Vincent Grande <shoober420@gmail.com>
2023-06-06 14:58:52 -0700

    Update enableui.bat

--- enableui.bat
+++ enableui.bat
@@ -10,8 +10,9 @@ net start UdkUserSvc
 rem Application Frame Host (to restore run "sfc /scannow")
 takeown /s %computername% /u %username% /f "%WinDir%\System32\ApplicationFrameHost.exe"
 icacls "%WinDir%\System32\ApplicationFrameHost.exe" /grant:r %username%:F
-taskkill /im ApplicationFrameHost.exe /f
+rem taskkill /im ApplicationFrameHost.exe /f
 rem del "%WinDir%\System32\ApplicationFrameHost.exe" /s /f /q
 ren "%WinDir%\System32\ApplicationFrameHost.exe.bak" "ApplicationFrameHost.exe"
+start "%WinDir%\System32" ApplicationFrameHost.exe
 
 PAUSE
