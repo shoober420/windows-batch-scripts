b859bce1ff6e66b1bf90205abe488dca2fb0de05
Vincent Grande <shoober420@gmail.com>
2023-06-06 15:37:11 -0700

    Update disableui.bat

--- disableui.bat
+++ disableui.bat
@@ -14,4 +14,11 @@ taskkill /im ApplicationFrameHost.exe /f
 rem del "%WinDir%\System32\ApplicationFrameHost.exe" /s /f /q
 ren "%WinDir%\System32\ApplicationFrameHost.exe" "ApplicationFrameHost.exe.bak"
 
+rem Search Host (to restore run "sfc /scannow")
+takeown /s %computername% /u %username% /f "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe"
+icacls "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /grant:r %username%:F
+taskkill /im SearchHost.exe /f
+rem del "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /s /f /q
+ren "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" "SearchHost.exe.bak"
+
 PAUSE
