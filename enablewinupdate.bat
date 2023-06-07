89b8506cc929dfd8debe66ad6d5f3edb2ff5bcf3
Vincent Grande <shoober420@gmail.com>
2023-06-07 12:51:05 -0700

    Update enablewinupdate.bat

--- enablewinupdate.bat
+++ enablewinupdate.bat
@@ -1,10 +1,11 @@
+rem Enable Windows Update
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\UsoSvc" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wisvc" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\wuauserv" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BITS" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\TrustedInstaller" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "2" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\InstallService" /v "Start" /t REG_DWORD /d "2" /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\InventorySvc" /v "Start" /t REG_DWORD /d "2" /f
 
 net start UsoSvc
@@ -14,7 +15,6 @@ net start BITS
 net start TrustedInstaller
 net start WaaSMedicSvc
 net start cryptsvc
-net start InstallService
 net start InventorySvc
 
 PAUSE
