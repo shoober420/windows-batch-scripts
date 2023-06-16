28fa5b663c66b616b5ac76d552df04b3b25bc5a0
Vincent Grande <shoober420@gmail.com>
2023-06-15 18:36:14 -0700

    Create vcredist.bat

---
+++ vcredist.bat
@@ -0,0 +1,17 @@
+cd C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.20.1081.0_x64__8wekyb3d8bbwe
+winget --info
+
+winget install --id=Microsoft.VC++2005Redist-x86 -e
+winget install --id=Microsoft.VC++2005Redist-x64 -e
+winget install --id=Microsoft.VC++2008Redist-x86 -e
+winget install --id=Microsoft.VC++2008Redist-x64 -e
+winget install --id=Microsoft.VC++2010Redist-x86 -e
+winget install --id=Microsoft.VC++2010Redist-x64 -e
+winget install --id=Microsoft.VC++2012Redist-x86 -e
+winget install --id=Microsoft.VC++2012Redist-x64 -e
+winget install --id=Microsoft.VC++2013Redist-x86 -e
+winget install --id=Microsoft.VC++2013Redist-x64 -e
+winget install --id=Microsoft.VC++2015-2022Redist-x86 -e
+winget install --id=Microsoft.VC++2015-2022Redist-x64 -e
+
+PAUSE
