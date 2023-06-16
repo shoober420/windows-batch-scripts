2fc33c404cf13adeaf5edff99bc772ceb3e5ae37
Vincent Grande <shoober420@gmail.com>
2023-06-15 18:43:27 -0700

    Update vcredist.bat

--- vcredist.bat
+++ vcredist.bat
@@ -1,17 +1,17 @@
 cd C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.20.1081.0_x64__8wekyb3d8bbwe
 winget --info
 
-winget install --id=Microsoft.VC++2005Redist-x86 -e
-winget install --id=Microsoft.VC++2005Redist-x64 -e
-winget install --id=Microsoft.VC++2008Redist-x86 -e
-winget install --id=Microsoft.VC++2008Redist-x64 -e
-winget install --id=Microsoft.VC++2010Redist-x86 -e
-winget install --id=Microsoft.VC++2010Redist-x64 -e
-winget install --id=Microsoft.VC++2012Redist-x86 -e
-winget install --id=Microsoft.VC++2012Redist-x64 -e
-winget install --id=Microsoft.VC++2013Redist-x86 -e
-winget install --id=Microsoft.VC++2013Redist-x64 -e
-winget install --id=Microsoft.VC++2015-2022Redist-x86 -e
-winget install --id=Microsoft.VC++2015-2022Redist-x64 -e
+winget install --id=Microsoft.VCRedist.2005.x86 -e
+winget install --id=Microsoft.VCRedist.2005.x64 -e
+winget install --id=Microsoft.VCRedist.2008.x86 -e
+winget install --id=Microsoft.VCRedist.2008.x64 -e
+winget install --id=Microsoft.VCRedist.2010.x86 -e
+winget install --id=Microsoft.VCRedist.2010.x64 -e
+winget install --id=Microsoft.VCRedist.2012.x86 -e
+winget install --id=Microsoft.VCRedist.2012.x64 -e
+winget install --id=Microsoft.VCRedist.2013.x86 -e
+winget install --id=Microsoft.VCRedist.2013.x64 -e
+winget install --id=Microsoft.VCRedist.2015+.x86 -e
+winget install --id=Microsoft.VCRedist.2015+.x64 -e
 
 PAUSE
