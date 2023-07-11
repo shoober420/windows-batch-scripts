274c0d513262136061e7055920217005708dce00
Vincent Grande <shoober420@gmail.com>
2023-07-10 22:42:38 -0700

    Update enablebluetooth.bat

--- enablebluetooth.bat
+++ enablebluetooth.bat
@@ -1,20 +1,51 @@
 rem Enable Bluetooth
-net start bthserv
+
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\bthserv" /v "Start" /t REG_DWORD /d "2" /f
+net start bthserv
 
-net start CDPUserSvc
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPUserSvc" /v "Start" /t REG_DWORD /d "2" /f
+net start CDPUserSvc
 
-net start CDPSvc
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\CDPSvc" /v "Start" /t REG_DWORD /d "2" /f
+net start CDPSvc
 
-net start BthAvctpSvc
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthAvctpSvc" /v "Start" /t REG_DWORD /d "2" /f
+net start BthAvctpSvc
 
-net start DevicesFlowUserSvc
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DevicesFlowUserSvc" /v "Start" /t REG_DWORD /d "2" /f
+net start DevicesFlowUserSvc
 
-net start ClipSVC
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\ClipSVC" /v "Start" /t REG_DWORD /d "2" /f
+net start ClipSVC
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BTHPORT" /v "Start" /t REG_DWORD /d "2" /f
+net start BTHPORT
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BTHUSB" /v "Start" /t REG_DWORD /d "2" /f
+net start BTHUSB
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BluetoothUserService" /v "Start" /t REG_DWORD /d "2" /f
+net start BluetoothUserService
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BTAGService" /v "Start" /t REG_DWORD /d "2" /f
+net start BTAGService
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthA2dp" /v "Start" /t REG_DWORD /d "2" /f
+net start BthA2dp
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthEnum" /v "Start" /t REG_DWORD /d "2" /f
+net start BthEnum
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthHFEnum" /v "Start" /t REG_DWORD /d "2" /f
+net start BthHFEnum
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthLEEnum" /v "Start" /t REG_DWORD /d "2" /f
+net start BthLEEnum
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BthMini" /v "Start" /t REG_DWORD /d "2" /f
+net start BthMini
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\BTHMODEM" /v "Start" /t REG_DWORD /d "2" /f
+net start BTHMODEM
 
 PAUSE
